0caf974e521fa2a991038b5ec22fe47d6bb82583
mxuereb <mxuereb@shaw.ca>
2016-06-08 13:39:56 -0700

    Update to Dice functions
    Changed the rollUntilAllMatch to a rollUntilNumberOfKind where you can
    specific any number of kind that you want to match.

--- dice.js
+++ dice.js
@@ -5,7 +5,7 @@ function rollSingle(numberOfSides) {
 }
 console.log('roll a single 6-sided dice ->', rollSingle(6));
 
-//Tell it how many dice to roll and how many sides.  
+//Tell it how many dice to roll and how many sides.
 //It'll return an array of counts for each side.
 //For example, if I roll 10 6-sided dice a possible
 //result could be [2,3,1,0,2,2].
@@ -23,43 +23,49 @@ function rollMultiple(numberOfSides, numberOfDice) {
     }
     return counts;
 }
-console.log('roll 5, 6-sided dice ->', rollMultiple(6, 5));
- 
-//This function rolls a set of dice until they all match the supplied 
-//number. After each roll, it checks to see how many matches there were
-//and sets those dice aside.  This is repeated until all the dice match.
-//The result is the number of rolls it took to get them all to match.
-function rollUntilAllMatch(numberOfSides, numberOfDice, numberToMatch) {
+//console.log('roll 6 d6 and count results->', rollMultiple(6, 6));
 
-    var allMatch = false;
+//This function rolls a set of dice until they all match the supplied
+//number. After each roll, it checks to see how many matches there were
+//and sets those dice aside.  This is repeated until the specific number of dice match.
+//The result is the number of rolls it took to get the number of matches specificied.
+function rollUntilNumberOfKind(numberOfSides, numberOfDice, numberToMatch, numberOfKind) {
+
+    var foundMatch = false;
     var diceLeftToRoll = numberOfDice;
     var totalRolls = 0;
     var rollResult = [];
     var matches = 0;
-    
-    while (!allMatch) {
+
+    while (!foundMatch) {
         totalRolls++;
         rollResult = rollMultiple(numberOfSides, diceLeftToRoll);
         matches = rollResult[numberToMatch - 1];
         diceLeftToRoll = diceLeftToRoll - matches;
-        allMatch = diceLeftToRoll === 0;
-    }
-
+        if (matches => numberOfKind) {
+          foundMatch = true;
+        }
+        else {
+          foundMatch = (diceLeftToRoll === 0);
+        }
+    console.log('matches so far', matches, 'number of kind ', numberOfKind);;
+    console.log('dice left to roll', diceLeftToRoll);;
+    console.log('totalRolls', rollResult);;
     return totalRolls;
-}
-console.log('roll 5, 6-sided dice until they all match 1 ->', rollUntilAllMatch(6, 5, 1));
+    }
+console.log('roll 6 d6, until they there are at least 5 matches of 1 -> Totalrolls =', rollUntilAllMatch(6, 6, 1, 5));
 
 //This function just runs the above function as many times as you'd like and keeps
-//a running count of the number of rolls it took.  That total is then used to 
+//a running count of the number of rolls it took.  That total is then used to
 //determine the average number of rolls required
-function getAverageRollsToMatch(numberOfSides, numberOfDice, numberToMatch, loops) {
+function getAverageRollsToMatch(numberOfSides, numberOfDice, numberToMatch, numberOfKind, loops) {
     var i;
     var runningTotal = 0;
     for (i = 0; i < loops; i++) {
-        runningTotal += rollUntilAllMatch(numberOfSides, numberOfDice, numberToMatch);
+        runningTotal += rollUntilAllMatch(numberOfSides, numberOfDice, numberToMatch, numberOfKind);
     }
     return runningTotal / loops;
 }
 
-//This rolls 5 six-sided dice 10000 times.  It looks to match the number 1. 
-console.log('roll 5, 6-sided dice until they all match 1, do it 10000 times ->', getAverageRollsToMatch(6, 5, 1, 10000));
+//This rolls 5 six-sided dice 10000 times.  It looks to match the number 1.
+//console.log('roll 6 d6 until there are at least 3 matches, do it 10000 times ->', getAverageRollsToMatch(6, 6, 1, 1, 10000));
