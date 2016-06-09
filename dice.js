ebfc727a2cc52193daf7071177a73aa8000abaa5
mxuereb <mxuereb@shaw.ca>
2016-06-09 13:31:05 -0700

    Updated Dice functions
    Included a function to start with a roll pick the best match then try
    for X of a kind

--- dice.js
+++ dice.js
@@ -25,6 +25,7 @@ function rollMultiple(numberOfSides, numberOfDice) {
 }
 //console.log('roll 6 d6 and count results->', rollMultiple(6, 6));
 
+
 //This function rolls a set of dice until they all match the supplied
 //number. After each roll, it checks to see how many matches there were
 //and sets those dice aside.  This is repeated until the specific number of dice match.
@@ -62,16 +63,106 @@ function rollUntilNumberOfKind(numberOfSides, numberOfDice, numberToMatch, numbe
 function getAverageRollsToMatch(numberOfSides, numberOfDice, numberToMatch, numberOfKind, loops) {
     var i;
     var runningTotal = 0;
+
+    console.log('\nRoll', numberOfDice,'d',numberOfSides, 'until there are at least', numberOfKind, 'of a kind, over', loops, 'rolls');
     for (i = 0; i < loops; i++) {
         runningTotal += rollUntilNumberOfKind(numberOfSides, numberOfDice, numberToMatch, numberOfKind);
     }
     return runningTotal / loops;
 }
 
-//This rolls 5 six-sided dice 10000 times.  It looks to match the number 1.
-console.log('roll 6 d6 until there are at least 1 matches OF 1, do it 100000 times ->', getAverageRollsToMatch(6, 6, 1, 1, 100000));
-console.log('roll 6 d6 until there are at least 2 matches OF 1, do it 100000 times ->', getAverageRollsToMatch(6, 6, 1, 2, 100000));
-console.log('roll 6 d6 until there are at least 3 matches OF 1, do it 100000 times ->', getAverageRollsToMatch(6, 6, 1, 3, 100000));
-console.log('roll 6 d6 until there are at least 4 matches OF 1, do it 100000 times ->', getAverageRollsToMatch(6, 6, 1, 4, 100000));
-console.log('roll 6 d6 until there are at least 5 matches OF 1, do it 100000 times ->', getAverageRollsToMatch(6, 6, 1, 5, 100000));
-console.log('roll 6 d6 until there are at least 6 matches OF 1, do it 100000 times ->', getAverageRollsToMatch(6, 6, 1, 6, 100000));
+//This rolls 6 six-sided dice 100000 times.  It looks to match the number 1 a number of times.
+//console.log(getAverageRollsToMatch(6, 9, 1, 1, 100000));
+//console.log(getAverageRollsToMatch(6, 9, 1, 2, 100000));
+//console.log(getAverageRollsToMatch(6, 9, 1, 3, 100000));
+//console.log(getAverageRollsToMatch(6, 9, 1, 4, 100000));
+//console.log(getAverageRollsToMatch(6, 9, 1, 5, 100000));
+//console.log(getAverageRollsToMatch(6, 9, 1, 6, 100000));
+//console.log(getAverageRollsToMatch(6, 9, 1, 7, 100000));
+//console.log(getAverageRollsToMatch(6, 9, 1, 8, 100000));
+//console.log(getAverageRollsToMatch(6, 9, 1, 9, 100000));
+
+//This function returns the most matches regards of the specific face of the
+//dice rolled the once which will in turn be used as the icon to match to determine the
+//number of rolls for any icon of a kind which i susspect will be easier then
+//trying to roll a specific icon a number of times.
+function mostMatchesOnSingleRoll(numberOfSides,numberOfDice) {
+  var counts =[];
+  var mostDiceSide = 0;  //the 1 side on the dice
+  var i;
+
+  counts = rollMultiple(numberOfSides,numberOfDice);
+  //start at dice side 2 as 1 is automatically the most to begin
+  for (i = 1; i < numberOfSides; i++) {
+      if (counts[mostDiceSide] < counts[i]) {
+          mostDiceSide = i;
+      }
+  }
+  //console.log('Dice rolled', counts, 'most', mostDiceSide, counts[mostDiceSide]);
+  return counts[mostDiceSide];
+}
+//console.log(mostMatchesOnSingleRoll(6,6));
+
+//This function rolls a set of dice until they all match the supplied
+//number. After each roll, it checks to see how many matches there were
+//and sets those dice aside.  This is repeated until the specific number of dice match.
+//The result is the number of rolls it took to get the number of matches specificied.
+function rollUntilNumberOfKindWithSingleRoll(numberOfSides, numberOfDice, numberToMatch, numberOfKind, intialRollMatches) {
+
+    var foundMatch = false;
+    var diceLeftToRoll = numberOfDice;
+    var totalRolls = 1;
+    var rollResult = [];
+    var matches = 0;
+    var matchesTotal = 0;
+
+    if (intialRollMatches >= numberOfKind) {
+        return totalRolls;
+    }
+    else {
+        diceLeftToRoll = diceLeftToRoll - intialRollMatches;
+        matchesTotal = intialRollMatches;
+    }
+
+    while (!foundMatch) {
+        totalRolls++;
+        rollResult = rollMultiple(numberOfSides, diceLeftToRoll);
+        matches = rollResult[numberToMatch - 1];
+        matchesTotal += matches;
+        diceLeftToRoll = diceLeftToRoll - matches;
+        if (matchesTotal >= numberOfKind) {
+            foundMatch = true;
+        }
+        else {
+            foundMatch = (diceLeftToRoll === 0);
+        }
+    }
+    return totalRolls;
+}
+//console.log('roll 6 d6, until they there are at least 5 matches of 1 -> Totalrolls =', rollUntilNumberOfKind(6, 6, 1, 5));
+
+//This function just runs the above function as many times as you'd like and keeps
+//a running count of the number of rolls it took.  That total is then used to
+//determine the average number of rolls required after taking the best initial roll
+function getAverageRollsToMatchWithSeed(numberOfSides, numberOfDice, numberToMatch, numberOfKind, loops) {
+    var i;
+    var runningTotal = 0;
+    var matchesSingleRoll = 0;
+
+    console.log('\nRoll', numberOfDice,'d',numberOfSides, 'until there are at least', numberOfKind, 'of a kind not specific, over', loops, 'rolls');
+    for (i = 1; i < loops; i++) {
+        runningTotal += rollUntilNumberOfKindWithSingleRoll(numberOfSides, numberOfDice, numberToMatch, numberOfKind, mostMatchesOnSingleRoll(numberOfSides,numberOfDice));
+    }
+    return runningTotal / loops;
+}
+
+//This rolls 6 six-sided dice 100000 times.  It looks to match the number 1 a number of times.
+console.log(getAverageRollsToMatchWithSeed(6, 3, 1, 1, 100000));
+console.log(getAverageRollsToMatchWithSeed(6, 3, 1, 2, 100000));
+console.log(getAverageRollsToMatchWithSeed(6, 3, 1, 3, 100000));
+//console.log(getAverageRollsToMatchWithSeed(6, 4, 1, 4, 100000));
+//console.log(getAverageRollsToMatchWithSeed(6, 5, 1, 5, 100000));
+//console.log(getAverageRollsToMatchWithSeed(6, 6, 1, 6, 100000));
+//console.log(getAverageRollsToMatchWithSeed(6, 7, 1, 7, 100000));
+//console.log(getAverageRollsToMatchWithSeed(6, 8, 1, 8, 100000));
+//console.log(getAverageRollsToMatchWithSeed(6, 9, 1, 9, 100000));
